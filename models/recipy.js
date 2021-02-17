b3562c0c636c8c8616f5efe9fecd9b62899c786d
AJ <aj.monty@gmail.com>
2021-02-17 15:44:02 +0000

    crawler

--- models/recipy.js
+++ models/recipy.js
@@ -3,13 +3,51 @@ const Schema = mongoose.Schema;
 
 //create schema for todo
 const TodoSchema = new Schema({
-  action: {
-    type: String,
-    required: [true, "The todo text field is required"],
-  },
-});
+    name: {
+      type: String,
+      required: [true, "The name field is required"],
+    },
+    ingredients: {
+      type: Array,
+      required: [true, "The ingredients field is required"],
+    },
+    instructions: {
+      type: Array,
+      required: [true, "The instructions field is required"],
+    },
+    tags: {
+      type: Array,
+    },
+    time: {
+      prep: {
+        type: String,
+      },
+      cook: {
+        type: String,
+      },
+      active: {
+        type: String,
+      },
+      inactive: {
+        type: String,
+      },
+      ready: {
+        type: String,
+      },
+      total: {
+        type: String,
+      },
+    },
+    servings: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  });
 
 //create model for todo
 const Todo = mongoose.model("recipies", TodoSchema);
 
 module.exports = Todo;
+
