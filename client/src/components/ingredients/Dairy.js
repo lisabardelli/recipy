e49a0d607e110e20bf735538551fcd4f47eea2d3
ZeenLamDev <Zeen.Lam93@gmail.com>
2021-02-18 11:29:53 +0000

    added basic components for visuals

--- client/src/components/ingredients/Dairy.js
+++ client/src/components/ingredients/Dairy.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
-const DairyTab = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-    
-    const toggle = () => setIsOpen(!isOpen);
-    return (
-        <div>
-          <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-          <Collapse isOpen={isOpen}>
-            <Card>
-              <CardBody>
-              Anim pariatur cliche reprehenderit,
-               enim eiusmod high life accusamus terry richardson ad squid. Nihil
-               anim keffiyeh helvetica, craft beer labore wes anderson cred
-               nesciunt sapiente ea proident.
-              </CardBody>
-            </Card>
-          </Collapse>
-        </div>
-      );
-}
+import React, { useState } from "react";
+import { Collapse, Button, CardBody, Card } from "reactstrap";
+const Dairy = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default DairyTab
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div>
+      <Button color="primary" onClick={toggle} style={{ marginBottom: "1rem" }}>
+        Toggle
+      </Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
+  );
+};
+
+export default Dairy;
--- client/src/components/ingredients/IngredientList.js
+++ client/src/components/ingredients/IngredientList.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import DairyTab from './Dairy'
-
+import React from "react";
+import Dairy from "./Dairy";
+import Fish from "./Fish";
+import Meats from "./Meats";
+import Seafood from "./Seafood";
+import Vegetables from "./Vegetables";
+import Fruits from "./Fruits";
 
 const IngredientList = (props) => {
-    
   return (
-      <div>
-        <DairyTab/>
-      </div>
-  )
-}
+    <div>
+      <Dairy />
+      <Vegetables />
+      <Fruits />
+      <Meats />
+      <Fish />
+      <Seafood />
+    </div>
+  );
+};
 
-export default IngredientList
+export default IngredientList;
